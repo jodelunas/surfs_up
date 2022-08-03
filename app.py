a58c9e2b34483bd1585124c4d43260cced6e7c6b
Johannah <jodelunas@gmail.com>
2022-08-03 15:22:43 -0500

    minor change

--- app.py
+++ app.py
@@ -71,3 +71,5 @@ def stats(start=None, end=None):
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
     return jsonify(temps)
+if __name__ == "__main__":
+    app.run(debug=True)
